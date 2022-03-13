8b224c69500d141486f18fe724de7e934c85ccd2
agusand <amandreacchi@gmail.com>
2022-03-13 18:37:19 -0300

    Try

--- pages/api/cron/index.js
+++ pages/api/cron/index.js
@@ -1,8 +1,8 @@
 import Schedule from "../../../classes/Schedule.js";
 
+const schedule = new Schedule();
 export default function handler(req, res) {
 	if (req.method === "GET") {
-		const schedule = new Schedule();
 		res.status(200).send(schedule.state);
 	} else {
 		res.status(404).send("Not found");
